c21508543cc07c18407dc4ff2f7a6aae3b757c56
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-14 18:31:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function deleteFromDriverByKey(driver, key) {
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
-  delete driver.key;
+  driver = delete driver.key;
   return driver;
 }
